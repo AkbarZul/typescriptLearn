6d8ccac3d0e805e5c10f422583b00920c11e8359
akbar <akbar.zulfikar@batumbu.id>
2023-11-19 16:04:19 +0700

    Typescript Types

--- app.ts
+++ app.ts
@@ -7,22 +7,59 @@
 // document.body.appendChild(heading)
 
 interface product {
-    id: number,
-    name: string,
-    price: number
+  id: number;
+  name: string;
+  price: number;
 }
 
-function getProduct(id) : product {
-    return {
-        id: id,
-        name: `Awsome Gadget ${id}`,
-        price: 99.5
-    }
+function getProduct(id): product {
+  return {
+    id: id,
+    name: `Awsome Gadget ${id}`,
+    price: 99.5,
+  };
 }
 
 const showProduct = (name: string, price: number) => {
-    console.log(`The Product ${name} cost ${price}`)
-}
+  console.log(`The Product ${name} cost ${price}`);
+};
 
 const product = getProduct(1);
 showProduct(product.name, product.price);
+
+// let variablename: type;
+// let variablename: type = value;
+// const variablename: type = value;
+// let arrayName: type[]
+
+let counter: number;
+counter = 1;
+
+let counter2: number = 1;
+
+let names: string = "John Doe";
+let age: number = 12;
+let active: boolean = true;
+let user: string[] = ["John", "Doe", "Pater"];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "David",
+  age: 12,
+};
+
+// let greeting : (name: string) => string
+
+// greeting = function(name: string) {
+//     return `Hi ${name}`
+// }
+
+// greeting = function() {
+//     console.log("Hello");
+    
+// }
+
