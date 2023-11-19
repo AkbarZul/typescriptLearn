6b8433bce5500a3eee59083d92a3e5ec45635639
akbar <akbar.zulfikar@batumbu.id>
2023-11-19 15:15:12 +0700

    Typescript product

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-var message = "Hello Typescript!";
-var heading = document.createElement("h1");
-heading.textContent = message;
-document.body.appendChild(heading);
+// let message: string = "Hello Typescript!"
+function getProduct(id) {
+    return {
+        id: id,
+        name: "Awsome Gadget ".concat(id),
+        price: 99.5
+    };
+}
+var showProduct = function (name, price) {
+    console.log("The Product ".concat(product.name, " cost ").concat(product.price));
+};
+var product = getProduct(1);
+showProduct(product.name, product.price);
--- app.ts
+++ app.ts
@@ -1,7 +1,28 @@
-let message: string = "Hello Typescript!"
+// let message: string = "Hello Typescript!"
 
-let heading = document.createElement("h1")
+// let heading = document.createElement("h1")
 
-heading.textContent = message
+// heading.textContent = message
 
-document.body.appendChild(heading)
+// document.body.appendChild(heading)
+
+interface product {
+    id: number,
+    name: string,
+    price: number
+}
+
+function getProduct(id) : product {
+    return {
+        id: id,
+        name: `Awsome Gadget ${id}`,
+        price: 99.5
+    }
+}
+
+const showProduct = (name: string, price: number) => {
+    console.log(`The Product ${name} cost ${price}`)
+}
+
+const product = getProduct(1);
+showProduct(product.name, product.price);
