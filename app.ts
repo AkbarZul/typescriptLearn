48abb583a19b311207c69859aff80e0dcb209606
akbar <akbar.zulfikar@batumbu.id>
2023-11-23 11:06:41 +0700

    Tipe Function

--- app.ts
+++ app.ts
@@ -1,65 +1,126 @@
-// let message: string = "Hello Typescript!"
+// // let message: string = "Hello Typescript!"
 
-// let heading = document.createElement("h1")
+// // let heading = document.createElement("h1")
 
-// heading.textContent = message
+// // heading.textContent = message
 
-// document.body.appendChild(heading)
+// // document.body.appendChild(heading)
 
-interface product {
-  id: number;
-  name: string;
-  price: number;
-}
-
-function getProduct(id): product {
-  return {
-    id: id,
-    name: `Awsome Gadget ${id}`,
-    price: 99.5,
-  };
-}
-
-const showProduct = (name: string, price: number) => {
-  console.log(`The Product ${name} cost ${price}`);
-};
-
-const product = getProduct(1);
-showProduct(product.name, product.price);
-
-// let variablename: type;
-// let variablename: type = value;
-// const variablename: type = value;
-// let arrayName: type[]
-
-let counter: number;
-counter = 1;
-
-let counter2: number = 1;
-
-let names: string = "John Doe";
-let age: number = 12;
-let active: boolean = true;
-let user: string[] = ["John", "Doe", "Pater"];
-
-let person: {
-  name: string;
-  age: number;
-};
-
-person = {
-  name: "David",
-  age: 12,
-};
-
-// let greeting : (name: string) => string
-
-// greeting = function(name: string) {
-//     return `Hi ${name}`
+// interface product {
+//   id: number;
+//   name: string;
+//   price: number;
 // }
 
-// greeting = function() {
-//     console.log("Hello");
-    
+// function getProduct(id): product {
+//   return {
+//     id: id,
+//     name: `Awsome Gadget ${id}`,
+//     price: 99.5,
+//   };
 // }
+
+// const showProduct = (name: string, price: number) => {
+//   console.log(`The Product ${name} cost ${price}`);
+// };
+
+// const product = getProduct(1);
+// showProduct(product.name, product.price);
+
+// // let variablename: type;
+// // let variablename: type = value;
+// // const variablename: type = value;
+// // let arrayName: type[]
+
+// let counter: number;
+// counter = 1;
+
+// let counter2: number = 1;
+
+// let names: string = "John Doe";
+// let age: number = 12;
+// let active: boolean = true;
+// let user: string[] = ["John", "Doe", "Pater"];
+
+// const arrays: Array<string> = ["a", "b", "c"]
+
+// console.log(arrays);
+
+// let person: {
+//   name: string;
+//   age: number;
+// };
+
+// person = {
+//   name: "David",
+//   age: 12,
+// };
+
+// // let counter: number = 1
+
+// function setCounter (counter: number) {
+//     return counter++
+// }
+
+// // same ad setCounter
+// function increment (counter: number) : number {
+//     return counter++
+// }
+
+// function parameter
+function logAgeandName(age: number, name = "anonymous") {
+  console.log(`${name}, age ${age}`);
+}
+
+logAgeandName(0);
+
+// function optional parameter
+function logFavoriteNumberAndReason(favorite: number, reason?: string) {
+  console.log(`Favorite: ${favorite}`);
+
+  if (reason) {
+    console.log(`Because: ${reason}`);
+  }
+}
+
+logFavoriteNumberAndReason(7, "test doang");
+
+// return types
+function getRandomFriend(fallback: string) {
+  switch (Math.floor(Math.random() * 5)) {
+    case 0:
+      return "Josh";
+    case 1:
+      return "sara";
+    case 2:
+      return "sonny";
+    default:
+      return fallback;
+  }
+}
+
+const friend = getRandomFriend("Codey")
+
+function fibonacci(i: number): number {
+  if (i <= 1) {
+    return i
+  }
+
+  return fibonacci(i-1) + fibonacci(i-2)
+}
+
+console.log(fibonacci(4));
+
+// Function Types
+let value = 0
+function withIncrementedValue(receiveNewValue: (newValue: number) => void) {
+  value += 3;
+  receiveNewValue(value)
+}
+
+withIncrementedValue((receivedValue) => {
+  console.log("Got", receivedValue);
+  
+})
+
 
